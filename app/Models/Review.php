9a04d91580a8e7c8018b0b17382fe4007824f369
fiftysev <nesgorai@bk.ru>
2022-03-02 01:15:48 +1000

    Add cast rating to double for Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -15,6 +15,10 @@ class Review extends Model
         'text'
     ];
 
+    protected $casts = [
+      'rating' => 'double'
+    ];
+
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
